ece70656648483ff3e19a5a00d00218b85939d2c
Mohammed Hannechi <mohammed.hannechi@gmail.com>
2016-04-06 20:59:44 +0100

    spring_client_device_detection

---
+++ spring_client_device_detection/README.md
@@ -0,0 +1,6 @@
+**Steps:**
+
+1- cd {PATH}/spring_messaging-rabbitmq
+
+2- mvn spring-boot:run
+
---
+++ spring_client_device_detection/src/main/java/com/medhan/spring/device_detection/Application.java
@@ -0,0 +1,7 @@
+package com.medhan.spring.device_detection;
+
+/**
+ * Created by mhan on 4/6/16.
+ */
+public class Application {
+}
